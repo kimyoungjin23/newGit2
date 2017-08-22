46bfa25248a0fdc41f09bab838d53ac71974da2e
Mycomputer\My computer <kim.youngjin23@gmail.com>
2017-08-22 11:48:39 -0400

    no message

--- YJ.js
+++ YJ.js
@@ -1,2 +1,14 @@
 console.log('hi jjae', Date())
 lala=999
+
+x.onkeyup = y.onkeyup = function (ev){
+
+    //console.log('you pressed', this.key)
+    x= parseFloat(document.getElementById('x').value)
+    y= parseFloat(document.getElementById('y').value)
+    alert(x)
+    
+}
+console.log(x+y)
+console.log(z)
+cbio.get()
