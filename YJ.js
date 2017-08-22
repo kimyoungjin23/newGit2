2790b7a11b51704fa91e5bff61ac84366b1e6a08
Mycomputer\My computer <kim.youngjin23@gmail.com>
2017-08-22 10:48:41 -0400

    no message

--- YJ.js
+++ YJ.js
@@ -1,10 +1,2 @@
 console.log('hi jjae', Date())
 lala=999
-/*
-
-parseFloat(x.value)
-*/
-console.log(x); 
-x.onkeyup=y.onkeyup=function(ev){
-    z.textContent=parseFloat(x.value)+parseFloat(y.value)
-}
